9ffdcfeaf860dc38d901b2f9242d7f9409965575
parvejali@0.com <83010920+Parvej0Ali@users.noreply.github.com>
2023-01-15 19:43:33 +0530

    Ecommerce

---
+++ index.js
@@ -0,0 +1,17 @@
+function below(){
+    alert('go down on the page for shopping chairs!');
+}
+function add(){
+    if (confirm("Click ok to buy the product?")){
+        alert('Thank you for the order, by Parvej Ali,\n your order will arrive in 4 days.');
+        }
+}
+function Home(){
+    alert('You are already at homepage');
+}
+function Contact(){
+    alert("Parvej Ali\n Phone number:639684****")
+}
+function About(){
+    alert("This is a E-commerce webpage made by Parvej Ali student of Indian Institute of Information Technology Kota.\n Here you can buy chairs which are comfortable and healthy.\n Click ok to know about me.")
+}
